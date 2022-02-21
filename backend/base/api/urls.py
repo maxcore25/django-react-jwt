c8d8a95f9ddb7bc3023b4fc37250add5bbd8b027
maxcore25 <mkorobov2002@mail.ru>
2022-02-21 17:37:12 +0300

    Custom Token Class Added

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -6,9 +6,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from .views import MyTokenObtainPairView
+
 urlpatterns = [
     path('', views.getRoutes),
 
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
