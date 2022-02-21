a1e2a31732be53feda3d7cba0c0b4d02f6d45681
maxcore25 <mkorobov2002@mail.ru>
2022-02-21 16:00:35 +0300

    Simple JWT Library registered

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
