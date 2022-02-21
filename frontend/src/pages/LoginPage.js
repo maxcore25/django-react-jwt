e27ddb5f8b838fab09b85c15daac83555105de62
maxcore25 <mkorobov2002@mail.ru>
2022-02-21 17:59:45 +0300

    Login Form created

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,7 +3,17 @@ import React from 'react';
 export default function LoginPage() {
   return (
     <div>
-      <p>login</p>
+      <form>
+        <div>
+          <div>Login</div>
+          <input type='text' name='username' />
+        </div>
+        <div>
+          <div>Password</div>
+          <input type='text' name='password' />
+        </div>
+        <button type='submit'>Log in</button>
+      </form>
     </div>
   );
 }
