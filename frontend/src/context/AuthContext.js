82283f63af325cbbda62086ea9b45d278ca9d31e
maxcore25 <mkorobov2002@mail.ru>
2022-02-22 05:49:07 +0300

    Callback In useState added For Less Rerenders

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -7,12 +7,12 @@ const AuthContext = createContext();
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(
+  const [user, setUser] = useState(() =>
     localStorage.getItem('authTokens')
       ? jwt_decode(localStorage.getItem('authTokens'))
       : null
   );
-  const [authTokens, setAuthTokens] = useState(
+  const [authTokens, setAuthTokens] = useState(() =>
     localStorage.getItem('authTokens')
       ? JSON.parse(localStorage.getItem('authTokens'))
       : null
