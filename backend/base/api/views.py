f9272fb949eb9af2266a47e54b007a54450f278c
maxcore25 <mkorobov2002@mail.ru>
2022-02-22 07:29:52 +0300

    Permission For Get Notes Endpoint added

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -35,7 +36,8 @@ def get_routes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_notes(request):
     notes = Note.objects.all()
-    serializer = NoteSerializer(notes,many=True)
+    serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
